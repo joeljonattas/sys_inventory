812022b20a12609e911d20091713ec9710ecf343
Joel Jonatas Arlt <joelarlt7@gmail.com>
2024-12-05 20:41:52 -0300

    Alterações no template

--- printers/models.py
+++ printers/models.py
@@ -19,7 +19,7 @@ class Printer(models.Model):
     name = models.CharField(max_length=50, blank=True)
     model = models.CharField(max_length=100)
     brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='printers_brand')
-    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='printers_category', default='3')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='printers_category', default='4')
     serie_number = models.CharField(max_length=50, blank=True, unique=True)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='in_stock')
     inventory_number = models.CharField(max_length=20, blank=True, unique=True)
